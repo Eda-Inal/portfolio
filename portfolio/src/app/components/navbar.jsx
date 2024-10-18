e3a2ab2178da93c3e23bf24a6bdd95930a8bbc9a
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-18 15:51:57 +0300

    fixed some design mistakes

--- portfolio/src/app/components/navbar.jsx
+++ portfolio/src/app/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='border-l text-2xl md:mt-8 mt-4 flex sm:ml-16 ml-8 md:ml-0' >
+        <div className='border-l text-lg sm:text-2xl md:mt-8 mt-4 flex sm:ml-16 ml-8 md:ml-0' >
             <div className='flex flex-col ml-4 '>
                 <div className='mb-2 font-light'>Home</div>
                 <div className='mb-2 font-light'>About</div>
--- portfolio/src/app/sidebar/page.jsx
+++ portfolio/src/app/sidebar/page.jsx
@@ -5,17 +5,20 @@ import Navbar from '../components/navbar'
 
 function SideBar() {
     return (
-        <div className='container md:w-2/5 w-4/5 sm:w-4/5 sm:h-48 h-36 mx-auto  md:mx-0  md:h-screen md:border-r-2 '  >
+        <div className='container md:w-2/5 w-5/5 sm:w-4/5 sm:h-48 h-36  md:mx-0  md:h-screen md:border-r-2 '  >
+            <div className='flex md:flex-col md:items-center w-full justify-around '>
+                <div className='  flex md:flex-col   flex-row items-center   sm:mx-auto md:mx-0 md:mt-8  ' >
 
-            <div className='  flex md:flex-col  md:items-center  flex-row items-center   sm:mx-auto md:mx-0  justify-center' >
+                    <Photo />
+                    <Links />
 
-                <Photo />
-                <Links />
-
-                <Navbar />
-
-
+                </div>
+                <div>
+                    <Navbar />
+                </div>
             </div>
+
+
 
 
         </div>
