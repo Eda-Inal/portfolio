4529db203e8111edec317ca81db2ab6e682885a0
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-18 12:54:12 +0300

    did photo component design and responsive design

--- portfolio/src/app/components/photo.jsx
+++ portfolio/src/app/components/photo.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import me from "../../../public/images/me.jpeg"
 
 function Photo() {
-  return (
-    <div>
-    
-    </div>
-  )
+    return (
+        <div className='sm:w-[180px] sm:h-[180px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px]  w-[120px] h-[120px] bg-white rounded-full sm:mt-10 mt-2 ml-2 sm:ml-6 md:ml-0 flex justify-center items-center' >
+            <div className='relative sm:w-[160px] sm:h-[160px] md:w-[180px] md:h-[180px] lg:w-[220px] lg:h-[220px] w-[100px] h-[100px] rounded-full overflow-hidden'>
+                <Image
+                    src={me}
+                    fill
+                    style={{ objectFit: 'cover', objectPosition: 'center' }}
+                    alt="me"
+                    priority
+                    sizes="(max-width: 220px) 100vw, 220px" 
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Photo
--- portfolio/src/app/sidebar/page.jsx
+++ portfolio/src/app/sidebar/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Photo from '../components/photo'
 
 function SideBar() {
   return (
     <div className='container md:w-2/5 w-4/5 h-40 md:h-screen border-r-2'  >
-    <div className='w-4/5  flex flex-col items-center' >
+    <div className='  flex flex-col  md:items-center ' >
  
+<Photo/>
 
     </div>
     </div>
