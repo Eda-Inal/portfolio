f5c5effe20108d5eaac127ead93a5d1b2a50dd0a
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-19 10:28:52 +0300

    set up layout with sidebar and dynamic content area

--- portfolio/src/app/components/navbar.jsx
+++ portfolio/src/app/components/navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navbar() {
     return (
         <div className='border-l text-lg sm:text-2xl md:mt-8 mt-4 flex sm:ml-16 ml-8 md:ml-0' >
             <div className='flex flex-col ml-4 '>
-                <div className='mb-2 font-light'>Home</div>
+                <div className='mb-2 font-light'><Link href="./home">Home</Link></div>
                 <div className='mb-2 font-light'>About</div>
                 <div className='mb-2 font-light'>Projects</div>
                 <div className=' font-light'>Contact</div>
--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -6,9 +6,11 @@ import SideBar from "./sidebar/page";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <SideBar/>
-        {children}
+      <body className="flex flex-col md:flex-row h-screen">
+        <SideBar />
+        <div className="flex-grow w-full p-4 overflow-y-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
