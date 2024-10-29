5331bc169fda505d2ea1e5ef063d47954e60b61c
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-29 10:56:59 +0300

    add navbar links border and home page did main page

--- portfolio/src/app/components/navbar.jsx
+++ portfolio/src/app/components/navbar.jsx
@@ -1,14 +1,21 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
+    const pathname = usePathname();
+    const isActive = (path) =>
+        pathname === path
+          ? 'text-secondary border-b-2 border-secondary'
+          : 'border-b-2 border-transparent'; 
     return (
         <div className='border-l text-sm sm:text-lg md:text-2xl md:mt-8 flex  md:ml-0' >
             <div className='flex flex-col ml-4 '>
-                <div className='mb-2 font-light'><Link href="./home">Home</Link></div>
-                <div className='mb-2 font-light'><Link href="./about">About</Link></div>
-                <div className='mb-2 font-light'><Link href="./projects">Projects</Link></div>
-                <div className=' font-light'><Link href="./contact">Contact</Link></div>
+                <div className={`mb-2 font-light ${isActive('/')}`}><Link href="./">Home</Link></div>
+                <div className={`mb-2 font-light ${isActive('/about')}`}><Link href="./about">About</Link></div>
+                <div className={`mb-2 font-light ${isActive('/projects')}`}><Link href="./projects">Projects</Link></div>
+                <div className={`mb-2 font-light ${isActive('/contact')}`}><Link href="./contact">Contact</Link></div>
             </div>
 
         </div>
