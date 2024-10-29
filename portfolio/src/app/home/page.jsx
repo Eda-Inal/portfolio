fc3ee128804f696d2fd5f82ffc7c757982c7c13c
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-29 15:40:52 +0300

    Update page.jsx

--- portfolio/src/app/home/page.jsx
+++ portfolio/src/app/home/page.jsx
@@ -10,16 +10,11 @@ function Home() {
         <div className='md:mt-8 w-4/5 mx-auto'>
             <div className='flex flex-col'>
                 <div><Header /> </div>
-                {/* <div className='md:mt-10 lg:mt-16 mt-8'><Languages /> </div> */}
-            
-                <div className='md:mt-10 lg:mt-16 mt-8'><Tools /> </div> 
-                <div className='mt-10'><Design/></div>
-         <ToolsImages/>
+                <div className='md:mt-10 lg:mt-16 mt-8'><Languages /> </div>
+                <div className='md:mt-10 lg:mt-16 mt-8'><Tools /> </div>
+                <div className='mt-10'><Design /></div>
+                <div className='mb-8'><ToolsImages /></div>
             </div>
-            {/* <button className='mt-8 border border-secondary p-3 rounded-md hover:bg-secondary hover:text-black md:text-lg text-sm sm:text-base   '>
-                Tools and Libraries
-            </button> */}
-
         </div>
     )
 }
