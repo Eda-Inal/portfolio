8573a3f2db7bbf768bb0c2987f05fa157778bc00
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-30 09:34:16 +0300

    add footer

---
+++ portfolio/src/app/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div  className='md:mt-8 w-4/5 mx-auto'>
+    © 2024 Eda Isil Inal
+    </div>
+  )
+}
+
+export default Footer
--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -1,15 +1,19 @@
 import "./globals.css";
 import SideBar from "./sidebar/page";
+import Footer from "./components/footer";
 
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col md:flex-row h-screen">
+      <body className="flex flex-col md:flex-row min-h-screen">
         <SideBar />
+        <div className="flex flex-col flex-grow w-full">
         <div className="flex-grow w-full p-4 overflow-y-auto">
           {children}
+        </div>
+        <Footer/>
         </div>
       </body>
     </html>
