777642bfc9441b195c1171e28dffdbafba8c80fe
Eda Işıl İnal <edainalisil@gmail.com>
2024-11-02 19:54:14 +0300

    remove border

--- portfolio/src/app/sidebar/page.jsx
+++ portfolio/src/app/sidebar/page.jsx
@@ -5,7 +5,7 @@ import Navbar from '../components/navbar'
 
 function SideBar() {
     return (
-        <div className='container md:w-2/5 w-4/5 sm:w-4/5 sm:h-48 h-36  md:mx-0  md:h-screen md:border-r-2 mx-auto '   >
+        <div className='container md:w-2/5 w-4/5 sm:w-4/5 sm:h-48 h-36  md:mx-0  md:h-screen mx-auto '   >
             <div className='flex md:flex-col md:items-center w-full justify-between sm:justify-between mt-4 md:mt-0 '>
                 <div className='  flex md:flex-col   flex-row items-center   md:mx-0 md:mt-8  ' >
 
