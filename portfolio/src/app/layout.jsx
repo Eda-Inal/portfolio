ff46a3423ad043b16dc3844a743f5d614267f8b8
Eda Işıl İnal <edainalisil@gmail.com>
2024-11-02 15:43:56 +0300

    add page transitions with framer motion

--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import SideBar from "./sidebar/page";
-
+import Template from "./template";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
       <body className="flex flex-col md:flex-row h-screen">
         <SideBar />
         <div className="flex-grow w-full p-4 overflow-y-auto">
-          {children}
+        <Template>{children}</Template>
         </div>
         <div id="notification-root"></div>
       </body>
